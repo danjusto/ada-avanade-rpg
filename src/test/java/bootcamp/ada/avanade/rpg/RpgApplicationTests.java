6d552f6cc8d5a2a71a3235a92f02a0d53955b978
Daniel Justo <justodaniel29@gmail.com>
2023-09-10 10:15:31 -0300

    initializing project

---
+++ src/test/java/bootcamp/ada/avanade/rpg/RpgApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.ada.avanade.rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
